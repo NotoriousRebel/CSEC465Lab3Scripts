d4d75dfbf7b312d383799cee8b3ff2d3bd4e12be
Aaron Karenchak <ajk4677@rit.edi>
2019-10-03 14:44:28 -0400

    First Commit

---
+++ ostester/ostester.ps1
@@ -0,0 +1,26 @@
+$array =@()
+ForEach($line in Get-Content $args[0]){
+	$array = $array + $line
+}
+
+$count = $array.Count
+# $count
+For($i=0; $i -lt $count; $i++){
+	$output1= ping $array[$i] -n 1;
+	$output2= $output1 -match "TTL";
+	$output3=$output2 -split 'TTL='; 
+	$TTL=$output3[1];
+	# $TTL 
+	if($TTL -eq 128){
+		Write-Host $array[$i].ToString() '= Windows'
+	}
+	elseif($TTL -eq 64){
+		Write-Host $array[$i].ToString() '= Linux'
+	}
+	elseif($TTL -gt 0){
+		Write-Output 'Unknown OS'
+	}
+}
+
+
+# & 'C:\Users\Aaron Karenchak\Desktop\College\Audit\ostester.ps1'
