30a132ce37b29b27b5593b97ece80773dd475ac6
Zack Cogswell <zachary.s.cogswell@gmail.com>
2019-10-04 12:08:59 -0400

    Created a portscan test for Google's webservers.

---
+++ portscan/googletest.ps1
@@ -0,0 +1,3 @@
+# Tests portscan.ps1 on some of Google's webservers on near the HTTP and SSL ports.
+"Testing google.com at 172.217.6.236/30 on ports 80-90,440-450."
+.\portscan.ps1 172.217.6.236/30 80-90,440-450
