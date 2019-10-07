9ec452615bd5a932ffa7a9ea6aea9f856870e520
NotoriousRebel <matt12299@yahoo.com>
2019-10-07 09:46:09 -0400

    Updated file to conform to pep8 standards as well have more verbose output.

--- axfrtest/axfrtest.py
+++ axfrtest/axfrtest.py
@@ -11,48 +11,64 @@ import dns.resolver
 import dns.query
 import dns.zone
 
+
 def parse_arguments():
-    """Runs argparse
+    """
+    Runs argparse
 
     Returns:
         argparse.Namespace: collection of args from cmd line
     """
-    parser = ArgumentParser(description='Test a domain for AXFR zone transfers.')
-    parser.add_argument('domain', help='name of the domain to be tested')
+    parser = ArgumentParser(
+        description='Test a domain for AXFR zone transfers.')
+    parser.add_argument(
+        '-d',
+        '--domain',
+        help='name of the domain to be tested',
+        type=str,
+        required=True)
     args = parser.parse_args()
     return args
 
-def axfrtest(domain):
-    """Finds all nameservers registered to a domian and asks for an AXFR zone
+
+def axfrtest(domain: str):
+    """
+    Finds all nameservers registered to a domian and asks for an AXFR zone
         transfer from each.
-
     Prints if a nameserver allowed AXFR, passes otherwise.
-
-    Args:
+    @:param
         domain (str): domain and TLD to be tested
     """
     try:
         nameservers = dns.resolver.query(domain, 'NS')
+        # get list of nameservers for given domain
         for nameserver in nameservers.rrset:
             nameserver = str(nameserver)
             if nameserver and nameserver != "":
                 axfr = dns.query.xfr(nameserver, domain, lifetime=5)
                 try:
                     zone = dns.zone.from_xfr(axfr)
+                    # attempt axfr record transfer
                     if zone:
-                        print('{} allowed AXFR'.format(nameserver))
+                        print(f'AXFR Transfer Allowed for {zone}')
+                        print('Printing contents of record')
+                        for name, node in zone.nodes.items():
+                            rdatasets = node.rdatasets
+                            print('\n'.join(map(str, rdatasets)))
                 except (dns.exception.FormError, dns.query.TransferError, EOFError):
-                    continue
+                    print(f'No information found for: {nameserver}')
     except dns.resolver.NXDOMAIN:
         print('Error: NXDOMAIN -- the query name does not exist')
 
-def main():
-    """Main function
 
+def main():
+    """
+    Main function
     Parses arguments and passes to axfrtest
     """
     args = parse_arguments()
     axfrtest(args.domain)
+
 
 if __name__ == '__main__':
     main()
